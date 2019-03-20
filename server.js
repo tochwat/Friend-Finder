f08516e8dc4f14d0748c9830ff9ce3635ffc53d2
tochwat <tochwat@gmail.com>
2019-03-19 18:55:01 -0700

    working initial connection to backend

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var bodyParser = require('body-parser')
 //path is a built in node module so you dont need to install it, just require it
 var path = require('path');
 
+//load in the express library into the app variable, creating a new instance of express
 var app = express()
 var PORT = process.env.PORT || 8080
  
- 
+// server static assets
+// app.use(express.static("public"));
+
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
