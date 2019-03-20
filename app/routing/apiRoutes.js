f53a97dcc2d18ab543a6d5a94fe3dd9d2fae7d39
tochwat <tochwat@gmail.com>
2019-03-19 20:55:44 -0700

    initial modal working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,7 @@
 //these variables connect each of the responses below to the api routes and their corresponding json data
 var friendData = require('../data/friends');
+// var fs = require("fs");
+
  
 module.exports = function(app){
 
@@ -10,6 +12,10 @@ module.exports = function(app){
     app.post('/api/friends', function (req, res) {
         friendData.push(req.body);
         res.json(true);
+        // fs.appendFile('../data/friends', req.body, function () {
+        //     res.end();
+        // });
+        
     });
 
     // app.post('/api/clear', function() {
