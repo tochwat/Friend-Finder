f9d6692f40993b52202f799235584ec361b0d6a9
tochwat <tochwat@gmail.com>
2019-03-21 18:42:19 -0700

    backend code for matching works

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,14 +4,14 @@ var friendArray = [
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Aladdin_Disney_pose.png/220px-Aladdin_Disney_pose.png",
         scores: [
             "1",
-            "3",
-            "4",
             "1",
-            "5",
-            "3",
             "1",
-            "2",
-            "2",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
             "1"
         ]
     },
@@ -19,16 +19,16 @@ var friendArray = [
         name: "Wall-E",
         photo: "https://vignette.wikia.nocookie.net/disney/images/2/2b/Wall-E.png",
         scores: [
-            "1",
-            "3",
-            "4",
-            "4",
             "5",
-            "3",
-            "4",
-            "2",
-            "2",
-            "4"
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
         ]
     },
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,28 +10,45 @@ module.exports = function(app){
     });
 
     app.post('/api/friends', function (req, res) {
-        console.log(req.body);
+        // console.log(req.body);
+
         let topMatch = {
             name: '',
             photo: '',
-            totalDiff: 0
+            matchDiff: 1000
         };
         let userInput = req.body;
-        console.log(`User's name is: ${userInput.name}`);
-        console.log(`User's scores are: ${userInput.scores}`);
 
+        // console.log(`User's name is: ${userInput.name}`);
+        // console.log(`User's scores are: ${userInput.scores}`);
+
+        let totalDiff = 0;
+        //loop through the entire friendData array
+        for (let i=0; i< friendData.length; i++){
+
+            //inner loop - loop across friend's scores to get absolute value of score difference
+            for (let j=0; j<friendData[i].scores.length; j++) {
+                totalDiff += Math.abs(userInput.scores[j] - friendData[i].scores[j]);
+                console.log(`TotalDiff is: ${totalDiff}`);
+            }
+
+            //if totalDiff < topMatch.matchDiff , then set topMatch to the friendData[i] including the matchDiff amount
+            if (totalDiff < topMatch.matchDiff) {
+                topMatch.name = friendData[i].name;
+                topMatch.photo = friendData[i].photo;
+                topMatch.matchDiff = totalDiff;
+            }
+            //reset totalDiff before the next outer loop
+            totalDiff = 0;
+        }
+
+        console.log(`Your top match is ${topMatch.name}`);
+
+        
+        //Push the user's input into the friends.js array of data
         friendData.push(req.body);
         res.json(true);
-        // fs.appendFile('../data/friends', req.body, function () {
-        //     res.end();
-        // });
         
     });
-
-    // app.post('/api/clear', function() {
-    //     friends = [];
-
-    //     console.log(friendData);
-    // });
 
 }
