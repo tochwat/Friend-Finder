6e05d54bc32d9c68bf4c333c2f2d1c9299a32244
tochwat <tochwat@gmail.com>
2019-03-21 21:21:35 -0700

    working v1

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,12 +5,12 @@ var friendArray = [
         scores: [
             "1",
             "1",
+            "2",
+            "1",
+            "3",
             "1",
             "1",
-            "1",
-            "1",
-            "1",
-            "1",
+            "4",
             "1",
             "1"
         ]
@@ -19,16 +19,48 @@ var friendArray = [
         name: "Wall-E",
         photo: "https://vignette.wikia.nocookie.net/disney/images/2/2b/Wall-E.png",
         scores: [
+            "2",
+            "5",
+            "2",
             "5",
             "5",
+            "1",
+            "1",
             "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
+            "3",
             "5"
+        ]
+    },
+    {
+        name: "Woody",
+        photo: "https://upload.wikimedia.org/wikipedia/en/0/01/Sheriff_Woody.png",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "2",
+            "2",
+            "4",
+            "2",
+            "5",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "Donald",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Donald_Duck.svg/220px-Donald_Duck.svg.png",
+        scores: [
+            "5",
+            "4",
+            "1",
+            "2",
+            "4",
+            "2",
+            "4",
+            "3",
+            "4",
+            "4"
         ]
     },
 ];
